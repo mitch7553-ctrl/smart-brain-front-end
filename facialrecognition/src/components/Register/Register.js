def50b1cb8b185804590137f77e290d9386acad6
Mitchell Brown <mitch7553@gmail.com>
2026-02-16 13:57:21 +0800

    Added the backend url to the frontend.

--- facialrecognition/src/components/Register/Register.js
+++ facialrecognition/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://backend-w6n9.onrender.com/register', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
